823eca24f47828aff56226f090b25dfcac4bb406
hukla <129692708+huklaa@users.noreply.github.com>
2026-08-13 23:18:30 +0300

    use requested feather logo in Chainling cards

--- demo/app.js
+++ demo/app.js
@@ -22,3 +22,10 @@ if(window.ethereum?.on){window.ethereum.on("accountsChanged",(accounts)=>{accoun
 renderCardPrices();
 renderSelectedPrice();
 updateWalletUi();
+
+const featherCss=document.createElement("style");
+featherCss.textContent=`
+.feather::after,.feather-mark::after,.sigil::after,.benefits div:first-child b::after{content:""!important;display:inline-block;background:currentColor!important;-webkit-mask:url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 120 120'%3E%3Cpath fill='black' d='M92 8C62 16 34 35 18 60 8 75 6 91 11 106c1 4 6 5 9 2l25-22-18 26c-1 2 0 5 2 6 2 1 4 1 6-1l20-20 6 12 10-28-19-1 24-12-14-5 28-12-16-5 21-9-14-4c9-7 16-15 20-23 2-5-3-10-9-8z'/%3E%3Cpath fill='black' d='M18 106c15-20 32-37 51-52 9-7 18-13 27-18-8 7-15 14-22 21-18 18-34 37-47 57z'/%3E%3C/svg%3E") center/contain no-repeat!important;mask:url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 120 120'%3E%3Cpath fill='black' d='M92 8C62 16 34 35 18 60 8 75 6 91 11 106c1 4 6 5 9 2l25-22-18 26c-1 2 0 5 2 6 2 1 4 1 6-1l20-20 6 12 10-28-19-1 24-12-14-5 28-12-16-5 21-9-14-4c9-7 16-15 20-23 2-5-3-10-9-8z'/%3E%3Cpath fill='black' d='M18 106c15-20 32-37 51-52 9-7 18-13 27-18-8 7-15 14-22 21-18 18-34 37-47 57z'/%3E%3C/svg%3E") center/contain no-repeat!important;vertical-align:middle}
+.feather::after{width:26px;height:30px}.feather-mark::after{width:82px;height:92px}.sigil::after{width:70px;height:78px}.benefits div:first-child b::after{width:23px;height:27px}.art .sigil,.orbit .feather-mark{display:grid;place-items:center}
+`;
+document.head.appendChild(featherCss);
