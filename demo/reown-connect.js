9c6c8730b4dbca640db69c5ee760901b68b94fd5
hukla <hukla25@gmail.com>
2026-08-15 00:13:17 +0300

    Bundle one Reown instance for mobile wallets

--- demo/reown-connect.js
+++ demo/reown-connect.js
@@ -1,6 +1,6 @@
-import {createAppKit} from "https://esm.sh/@reown/appkit?bundle";
-import {EthersAdapter} from "https://esm.sh/@reown/appkit-adapter-ethers?bundle";
-import {defineChain} from "https://esm.sh/@reown/appkit/networks?bundle";
+import {createAppKit} from "@reown/appkit";
+import {EthersAdapter} from "@reown/appkit-adapter-ethers";
+import {defineChain} from "@reown/appkit/networks";
 
 const PROJECT_ID=window.CHAINLING_WALLETCONNECT_PROJECT_ID;
 const RPC_URL="https://rpc.mainnet.chain.robinhood.com/";
