8081050d905812291ffbabca8a328a7bfdcc8cb5
hukla <129692708+huklaa@users.noreply.github.com>
2026-08-18 01:49:21 +0300

    Route Home to the ecosystem landing

--- demo/landing.js
+++ demo/landing.js
@@ -1,3 +1,4 @@
-import "./landing-core.js?v=20260818-0139";
-import "./opening-copy.js?v=20260818-0139";
-import "./feather-fix.js?v=20260818-0139";
+import "./landing-core.js?v=20260818-0148";
+import "./opening-copy.js?v=20260818-0148";
+import "./feather-fix.js?v=20260818-0148";
+import "./home-anchor-fix.js?v=20260818-0148";
